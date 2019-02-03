55516920fab71451e27b7e4c96b052cc9516c830
megmotion <33497517+megmotion@users.noreply.github.com>
2019-02-03 03:54:38 +0100

    Change in modelss.py

--- blog/views.py
+++ blog/views.py
@@ -9,4 +9,4 @@ def post_list(request):
 
 def post_detail(request, pk):
 	post = get_object_or_404(Post, pk=pk)
-	return redirect('blog.views.post_detail', pk=post.pk)
+	return render(request, 'blog/post_detail.html', {'post': post})
