00d98fe6582d125eecb6effbb17d6144b66cc3cb
megmotion <33497517+megmotion@users.noreply.github.com>
2019-05-10 02:49:27 +0200

    changing setting.py static

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,8 +124,8 @@ MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+#STATICFILES_DIRS = [
+    #os.path.join(BASE_DIR, "static"),
     #'/var/www/static/',
 ]
 
